63800c2723d8ba1e975774a3c1e894d93aa5549a
Pedro H K Goncalves <108807271+PedroKons@users.noreply.github.com>
2022-08-11 23:00:00 -0300

    Create Ex7.cpp
    . Faça um programa que solicita um número inteiro e exibe uma mensagem indicando se ele é
    positivo, negativo ou zero. Saídas:
    • Pedido ao usuário = "Digite um número inteiro:";
    • Caso ele seja positivo = “Ele é positivo”;
    • Caso ele seja negativo = “Ele é negativo”;
    • Caso ele seja igual a zero = “Ele é igual a zero”.

---
+++ Desvio Condicional/Ex7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+  int num;
+
+  std::cout<<"Digite um valor inteiro para saber se eh positivo, negativo ou nulo: ";
+  std::cin>>num;
+
+  if  (num>0){
+    std::cout<<"Esse numero eh positivo";
+  } else if (num<0){
+    std::cout<<"Esse numero eh negativo";
+    }
+    else {
+    std::cout<<"Esse numero eh nulo";
+    
+  }
+  
+    
+    
+}
